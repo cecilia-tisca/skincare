e9994fef2a67525ff2d8412ba03de2f09643bd34
Diana <190184@utags.edu.mx>
2023-02-27 23:47:03 -0600

    Creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: '', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
